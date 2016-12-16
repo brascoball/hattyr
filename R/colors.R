4371e51b02d8fc02ad4dc022c889b2a2b7fe19b2
Don Chesworth <dcheswor@redhat.com>
2016-12-16 13:38:59 -0500

    added show_redhat_colors()

--- R/colors.R
+++ R/colors.R
@@ -15,3 +15,37 @@ redhat_colors = function(color) {
   hex_number <- as.character(rh_colors[match(color, rh_colors$name),]$color)
   return(hex_number)
 }
+
+#' Show available Red Hat colors.
+#'
+#' (code primarily take from library(scales))
+#'
+#'
+#' @examples
+#' # Show all Red Hat colors
+#' show_redhat_colors()
+#'
+#' @export
+show_redhat_colors <- function() {
+
+  rh_hex <- rh_colors$color
+  n <- length(rh_hex)
+  ncol <- ceiling(sqrt(n))
+  nrow <- ceiling(n / ncol)
+  rh_hex <- c(rh_hex, rep(NA, nrow * ncol - length(rh_hex)))
+  rh_hex <- matrix(rh_hex, ncol = ncol, byrow = TRUE)
+
+  old <- par(pty = "s", mar = c(0, 0, 0, 0))
+  on.exit(par(old))
+
+  size <- max(dim(rh_hex))
+  plot(c(0, size), c(0, -size), type = "n", xlab="", ylab="", axes = FALSE)
+  rect(col(rh_hex) - 1, -row(rh_hex) + 1, col(rh_hex), -row(rh_hex),
+       col = rh_hex, border = '#FFFFFF')
+
+  rh_names <- gsub(" ", "\n", rh_colors$name)
+  rh_names <- c(rh_names, rep(NA, nrow * ncol - length(rh_names)))
+  rh_names <- matrix(rh_names, ncol = ncol, byrow = TRUE)
+  text(col(rh_hex) - 0.5, -row(rh_hex) + 0.5, rh_names, cex = 0.75)
+
+}
